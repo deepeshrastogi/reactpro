8d06da0fc2ac812264ade4a618eea993f8bf90c6
deepeshrastogi <deepesh.rastogi@maracana.in>
2024-04-11 11:15:42 +0530

    remove unwanted code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-import Form from "./components/Form";
 function App() {
-  return (
-    <div className="App">
-      <Form />
-    </div>
-  );
+  return <div className="App">Hello dev</div>;
 }
 
 export default App;
